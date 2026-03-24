563ca8a6bfdff5f7b63517a0e826c03ed5e3418e
anamika95341 <anamika95341@gmail.com>
2026-03-25 00:16:23 +0530

    deploying

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "ec2-13-201-12-198.ap-south-1.compute.amazonaws.com",
+    ],
+  },
+  preview: {
+    host: "0.0.0.0",
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "ec2-13-201-12-198.ap-south-1.compute.amazonaws.com",
+    ],
+  },
   resolve: {
     alias: {
       raf: fileURLToPath(new URL('./src/shared/vendor/raf.js', import.meta.url)),
